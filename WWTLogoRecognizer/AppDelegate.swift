1d319078ba00ef0b616deb202b148ad83448799d
Patrick Maltagliati <patmalt@gmail.com>
2017-10-18 22:15:34 -0500

    Attempt to start the downloading of models? it donsnt work

--- WWTLogoRecognizer/AppDelegate.swift
+++ WWTLogoRecognizer/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        ClarifaiWrapper.shared.start(key: "ef2e712921de4906a84991a81be80203")
+        
         return true
     }
 
--- WWTLogoRecognizer/ClarifaiWrapper.swift
+++ WWTLogoRecognizer/ClarifaiWrapper.swift
@@ -10,22 +10,45 @@ class ClarifaiWrapper {
     
     private init() {
         NotificationCenter.default.addObserver(self,
+                                               selector: #selector(willFetchModelNotification(notification:)),
+                                               name: NSNotification.Name.CAIWillFetchModel,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(didFetchModelNotification(notification:)),
+                                               name: NSNotification.Name.CAIDidFetchModel,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
                                                selector: #selector(modelDidBecomeAvailable(notification:)),
-                                               name: Notification.Name("CAIModelDidBecomeAvailable"),
+                                               name: NSNotification.Name.CAIModelDidBecomeAvailable,
                                                object: nil)
     }
     
-    func start(key: String = "") {
+    func start(key: String) {
+        print("Start With Key: \(key)")
         Clarifai.sharedInstance().start(apiKey: key)
     }
     
     @objc
+    private func willFetchModelNotification(notification: NSNotification) {
+        print("will fetch model")
+    }
+    
+    @objc
+    private func didFetchModelNotification(notification: NSNotification) {
+        print("will fetch model")
+    }
+    
+    @objc
     private func modelDidBecomeAvailable(notification: NSNotification) {
+        print("model did become available")
         guard let userInfo = notification.userInfo,
             let modelId = userInfo[CAIModelUniqueIdentifierKey] as? String else
         {
             return
         }
+        print("model id: \(modelId)")
         self.modelId = modelId
     }
 }
